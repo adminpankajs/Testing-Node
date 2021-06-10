e1cb959a941d373a27af99c1727a19f761938d43
shivam-it <52482326+shivam-it@users.noreply.github.com>
2021-06-10 22:27:42 +0530

    Done

--- NodeJs/app.js
+++ NodeJs/app.js
@@ -1,8 +1,1 @@
-const myModule_1 = require('./myModule-1')
-const myModule_2 = require('./myModule-2')
-const myModule_3 = require('./myModule_3')
-
-
-console.log(myModule_1.username)
-myModule_2.sayHi('Gupta Ji')
-console.log(myModule_3.singlePerson.name)
+hello
