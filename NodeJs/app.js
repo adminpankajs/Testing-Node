9c18fbc1dd6663ec7d031c3a88dfd3ee039eb4aa
shivam-it <52482326+shivam-it@users.noreply.github.com>
2021-06-11 09:59:33 +0530

    test file created

--- NodeJs/app.js
+++ NodeJs/app.js
@@ -1,1 +1,7 @@
-change it now
+const path = require('path')
+
+const filePath = path.join('/module','submodule')
+// console.log(filePath);
+
+const absolute = path.resolve(__dirname,'module','submodule','test.txt')
+console.log(absolute);
