89d1a2331a7cfc05b350c073ca327d83b8e2e0a8
shivam-it <52482326+shivam-it@users.noreply.github.com>
2021-06-11 12:32:51 +0530

    Learn : readFileSync

--- NodeJs/app.js
+++ NodeJs/app.js
@@ -1,8 +1,27 @@
-const path = require('path')
+const {readFile, writeFile} = require('fs')
 
-const filePath = path.join('/module','submodule')
-// console.log(filePath);
+console.log('start');
+readFile('./module/subModule/test_1.txt','utf8',(err,result) => {
+    if(err){
+        console.log(err)
+        return
+    }
+    const result_1 = result
+    readFile('./module/subModule/test_2.txt','utf8',(err,result)=>{
+        if(err){
+            console.log(err)
+            return
+        }
+        const result_2 = result
+        // console.log(`Ans is : ${result_1} ${result_2}`)
+        writeFile('./module/subModule/test.txt',`\nMy New Result is : ${result_1} ${result_2}`,{flag : 'a'},(err,result)=>{
+            if(err){
+                console.log(err)
+            }
+            console.log('Writing is done')
+        })
+    })
+})
 
-// Get file base directory
-const absolute = path.resolve(__dirname,'module','submodule','test.txt')
-console.log(absolute);
+console.log('done with task')
+console.log('starting the next')
