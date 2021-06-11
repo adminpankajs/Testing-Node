c3a1ebceefa345def3e831fffc5d8d3a9b507933
shivam-it <52482326+shivam-it@users.noreply.github.com>
2021-06-11 13:54:03 +0530

    Modules_added

---
+++ NodeJs/http.js
@@ -0,0 +1,18 @@
+const http = require('http')
+
+const server = http.createServer((req,res)=>{
+    if(req.url === '/')
+    {
+        res.end('Welcome ji');
+    }
+    if(req.url === '/about')
+    {
+        res.end('You are in the about');
+    }
+    res.end('Error ho gy !!')
+    
+})
+
+server.listen(5000,()=>{
+    console.log(`Server is listening....`);
+})
