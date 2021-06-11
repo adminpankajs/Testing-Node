d20f2cb38083f2026b0af7c780e262f18c5b7cb3
shivam-it <52482326+shivam-it@users.noreply.github.com>
2021-06-11 10:16:32 +0530

    my commit

--- NodeJs/app.js
+++ NodeJs/app.js
@@ -3,6 +3,6 @@ const path = require('path')
 const filePath = path.join('/module','submodule')
 // console.log(filePath);
 
-// Get file base dir
+// Get file base directory
 const absolute = path.resolve(__dirname,'module','submodule','test.txt')
 console.log(absolute);
